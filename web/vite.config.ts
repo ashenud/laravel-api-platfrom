69e14cc0fbe46ae58d3cfbd3b3ac0691b7564308
Ashen Udithamal <udithamal.lk@gmail.com>
2024-12-18 23:28:50 +0530

    Add VueJs files

---
+++ web/vite.config.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 5173, // Ensure the port matches the exposed Docker port
+  },
+})
